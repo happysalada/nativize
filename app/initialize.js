84c0af0786b94c9f93a3b3fc8b2071b77474fd23
happysalada <raphael.megzari@gmail.com>
2018-01-08 13:35:10 +0100

    use es7

--- app/initialize.js
+++ app/initialize.js
@@ -1,24 +1,28 @@
 import * as edn from 'jsedn'
 import axios from 'axios'
+import "babel-polyfill"
 
 document.addEventListener('DOMContentLoaded', () => {
   let submitTimeoutID;
-  const inputSource = document.querySelectorAll('#input-source')[0];
-  const nativizedResponse = document.querySelectorAll('#nativized-response')[0];
-  const nativizeButton = document.querySelectorAll('#nativize-button')[0];
-  const submit = () => inputSource.value &&
-    axios({
-      method: 'post',
-      url: 'https://spike-ring.work/',
-      headers: {'Content-Type': 'application/edn'},
-      data: edn.encode(new edn.Map([edn.kw(":text"), `${inputSource.value}`]))
-    }).then(({ data, status }) => {
+  const inputSource = document.getElementById('input-source')
+  const nativizedResponse = document.getElementById('nativized-response')
+  const nativizeButton = document.getElementById('nativize-button')
+  const submit = async () => {
+    if (!inputSource.value) return
+    try {
+      const { data, status} = await axios({
+        method: 'post',
+        url: 'https://spike-ring.work/',
+        headers: {'Content-Type': 'application/edn'},
+        data: edn.encode(new edn.Map([edn.kw(":text"), `${inputSource.value}`]))
+      })
       if (status > 299) throw new Error('there was a problem with the request')
       nativizedResponse.innerHTML = edn.parse(data).at(edn.kw(":text"))
       nativizedResponse.style.visibility = 'visible'
-    }).catch(error => {
+    } catch (error) {
       nativizedResponse.innerHTML = error.message
-    });
+    }
+  }
   inputSource.addEventListener('input', () => {
     if (submitTimeoutID) window.clearTimeout(submitTimeoutID);
     submitTimeoutID = window.setTimeout(submit, 1000);
